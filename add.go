1efd343a1fc0a44b61c902f964e7d3e4dde93d9d
WangLiang <if@cgolang.com>
2013-02-18 09:33:33 +0800

    Update add.go

--- add.go
+++ add.go
@@ -3,4 +3,15 @@ package sokeng
 import (
     "database/sql"
     "html/template"
+    "strings"
 )
+
+type add struct{
+        
+}
+
+func (this *add) Add(w http.ResponseWriter, r *http.Request){
+
+}
+
+
