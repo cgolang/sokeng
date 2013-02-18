3491d877a691593717c72b588dc286c5bbc1c5e3
WangLiang <if@cgolang.com>
2013-02-18 09:08:34 +0800

    Update router.go

--- router.go
+++ router.go
@@ -16,15 +16,17 @@ func (p *SoKengMux) ServeHTTP(w http.ResponseWriter, r *http.Request) Handle{
 
 	if r.URL.Path =="/add/" { //当用户提交新坑
 		add(w, r)
-	)
+	}
 
-	if r.URL.Path == "/ticket/" //当用户审核随机坑
+	if r.URL.Path == "/ticket/" { //当用户审核随机坑
+                ticket(w, r)
+        } 
+          
 
-	if regexp.MatchrString("/*/",r.URL.Path) { //当用户点击开始搜索
+	if regexp.MatchrString("^(\/s)(.*)",r.URL.Path) { //当用户点击开始搜索
 		index(w, r)
 		return
 	}
-
 
 	http.NotFound(w, r)
 	return
