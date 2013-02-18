98341946bf6e27293235d7276d785da537d7c9ef
WangLiang <if@cgolang.com>
2013-02-18 08:59:06 +0800

    Update router.go

--- router.go
+++ router.go
@@ -1,7 +1,31 @@
 package router
 
 import (
-      "net/http"
-      "regexp"
+    "fmt"
+    "net/http"
+    "regexp"
 )
 
+type SoKengMux struct{
+}
+
+func (p *SoKengMux) ServeHTTP(w http.ResponseWriter, r *http.Request) Handle{
+      if r.URL.Path == "/"{ //当用户打开首页
+
+	}
+
+	if r.URL.Path =="/add/" { //当用户提交新坑
+		add(w, r)
+	)
+
+	if r.URL.Path == "/ticket/" //当用户审核随机坑
+
+	if regexp.MatchrString("/*/",r.URL.Path) { //当用户点击开始搜索
+		index(w, r)
+		return
+	}
+
+
+	http.NotFound(w, r)
+	return
+}
