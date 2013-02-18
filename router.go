902a80c26406e32c5c499e880292d2f1163c7457
WangLiang <if@cgolang.com>
2013-02-18 09:15:46 +0800

    Update router.go

--- router.go
+++ router.go
@@ -21,6 +21,17 @@ func (p *SoKengMux) ServeHTTP(w http.ResponseWriter, r *http.Request) Handle{
 	if r.URL.Path == "/ticket/" { //当用户审核随机坑
                 ticket(w, r)
         } 
+
+	if r.URL.Path == "/admin/" { //当用户是管理组
+                admin(w, r)
+        }
+
+	if r.URL.Path == "/e/" { //当用户是企业用户
+                e8(w, r)
+        } 
+
+
+
           
 
 	if regexp.MatchrString("^(\/s)(.*)",r.URL.Path) { //当用户点击开始搜索
